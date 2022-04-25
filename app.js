d7ba2ba574adc3645f463aaa9493799b076ba387
Jason Scavenger <39012389+TrushinVlad@users.noreply.github.com>
2022-04-25 16:29:21 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
-const http = require('http');
-const port = process.env.PORT || 3000
+const http = require("http");
+const fs = require("fs");
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
-});
+http.createServer(async (request, response) => {
 
-server.listen(port,() => {
-  console.log(`Server running at port `+port);
-});
+  try {
+    let options = request.url.split(/number1=|&number2=/);
+    console.log("=== " + new Date() +" ===");
+    console.log(options[1]);
+    console.log(options[2]);
+    console.log("------------------------------------------\n");
+          if (options[0] == '/?') {
+          console.log(Number(options[1]) + Number(options[2]));
+          response.writeHead(200, {'Content-Type': 'application/json'});
+          let json = {
+                    name: 'VladTrue',
+                    number1: options[1],
+                    number2: options[2],
+                    sum: Number(options[1]) + Number(options[2]),
+                    min: Number(options[1]) - Number(options[2]),
+                    mul: Number(options[1]) * Number(options[2]),
+                    div: Number(options[1]) / Number(options[2])
+                  };
+          response.end(JSON.stringify(json));
+          }
+          else {
+            fs.readFile("index.html", (error, data) => response.end(data));
+          }
+  }
+  catch {
+          response.writeHead(400, {'Content-Type': 'application/json'});
+          let json = {
+            name: 'VladTrue',
+            desc: "Error 400"
+          };
+          response.end(JSON.stringify(json));
+  }
+
+
+}).listen(3000, () => console.log("Сервер запущен по адресу http://localhost:3000"));
